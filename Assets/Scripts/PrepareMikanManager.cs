ace4892872e4f32941b3459de6beeaa52a41c56d
noriakiumei <nori2022aki@gmail.com>
2025-10-09 19:56:01 +0900

    変更

--- Assets/Scripts/PrepareMikanManager.cs
+++ Assets/Scripts/PrepareMikanManager.cs
@@ -4,15 +4,18 @@ using UnityEngine.SceneManagement;
 public class ReadyAreaChecker : MonoBehaviour
 {
     [Header("プレイヤー")]
-    public Transform player;  
+    public Transform player;
 
     [Header("判定距離")]
     public float readyThreshold = 1.0f; // どれだけ近づいたら準備完了とするか
+
 
     [Header("次に進むシーン名")]
     public string nextSceneName = "PoleAvoid";
 
     private bool isReady = false;
+    private float stayTimer = 0f;//測る
+    public float stayTime = 3f; // プレイヤーがどんくらいいるか
 
     void Update()
     {
@@ -23,9 +26,18 @@ public class ReadyAreaChecker : MonoBehaviour
 
         if (distance < readyThreshold)
         {
-            Debug.Log("準備完了");
-            isReady = true;
-            SceneManager.LoadScene(nextSceneName);
+            stayTimer += Time.deltaTime;
+
+            if (stayTimer >= stayTime)
+            {
+                Debug.Log("準備完了");
+                isReady = true;
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+        else
+        {
+            stayTimer = 0f; // リセット
         }
     }
 }
